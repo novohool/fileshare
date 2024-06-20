0e556c75c5b3d2183649e60b805aa84fe95d7237
novohool <jingslunt@qq.com>
2024-06-20 14:42:21 +0800

    Update certsh.sh

--- certsh.sh
+++ certsh.sh
@@ -1,11 +1,10 @@
 certdir='/etc/kubernetes/ssl/'
 if [ -d /etc/kubernetes/certs/ ]
 then
-  certdir='/etc/kubernetes/certs/'
+	  certdir='/etc/kubernetes/certs/'
 fi
 
 for i in `ls $certdir|grep -v key`; do
-    openssl x509 -text -in ${certdir}$i | grep -A2 Validity |egrep "crt|After"| sed 's/^\([^:]*\): *\(.*\)/\2/' | while read -r date_str; do
-        echo "$(basename $i):$(date -d "${date_str#*: }" +'%Y-%m-%d.%H%M%S.GMT')"
-    done
+  enddate=$(openssl x509 -in ${certdir}$i -noout -enddate| sed 's/^\([^=]*\)= *\(.*\)/\2/'|xargs -i date -d "{}" +'%Y-%m-%d.%H%M%S.GMT')
+  echo $i:$enddate
 done
